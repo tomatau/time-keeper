15e10a405f5fd98a9875c63ba1af6d486189c9d5
Thomas Hudspith-Tatham <iammotivated@gmail.com>
2014-10-01 10:40:07 +0100

    added common modal directive

--- app/scripts/core/directives/modal.js
+++ app/scripts/core/directives/modal.js
@@ -1,9 +1,22 @@
 angular.module('core')
+    .factory('openModel', function(){
+        return function(ident){
+            angular.element('.' + ident).modal();
+        }
+    })
     .directive('tmModal', function(COREURL){
         return {
             templateUrl: COREURL + "/directives/modal.tmpl.html",
+            controllerAs: "modal",
+            bindToController: true,
+            transclude: true,
             scope: {
-                
-            }
+                id: "@ident",
+                header: "@header",
+                submitText: "@submitText",
+                submitFn: "&submitFn",
+            },
+            // need this for controllerAs despite bindToController
+            controller: function(){}
         }
-    })
+    });
