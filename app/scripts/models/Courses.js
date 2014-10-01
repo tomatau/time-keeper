c17e6e676531cf7779e2e2a384dc5444d3c0903c
Thomas Hudspith-Tatham <iammotivated@gmail.com>
2014-10-01 23:39:21 +0100

    added tests for course list

--- app/scripts/models/Courses.js
+++ app/scripts/models/Courses.js
@@ -3,8 +3,20 @@ angular.module('models')
     .factory('Courses', function(){
         var courseList = [];
         return {
-            add: function(){},
-            remove: function(){},
+            get: function(){
+                return courseList;
+            },
+            set: function(newList){
+                courseList.length = 0;
+                angular.extend(courseList,newList);
+            },
+            add: function(course){
+                courseList.push(course);
+            },
+            remove: function(course){ // keeps the reference
+                var idx = courseList.indexOf(course);
+                if ( idx > -1 ) courseList.splice(idx, 1);
+            },
             find: function(course){
                 return _.findWhere(courseList, course);
             }
