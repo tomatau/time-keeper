deb7a974418e390b8726b22e11575f4e835b79d6
Thomas Hudspith-Tatham <iammotivated@gmail.com>
2014-10-01 23:39:00 +0100

    testing ioc for gateways

--- app/tests/gateways/gateways-test.js
+++ app/tests/gateways/gateways-test.js
@@ -2,7 +2,7 @@ describe('Gateway Config', function () {
 
     beforeEach( module("gateways") );
 
-    it('should add providers for the gateways dependency map', 
+    it('should add injectables from the gateways dependency map', 
         inject(function(dependencyMap, $injector) {
             angular.forEach(dependencyMap.gateways, function(source, target){
                 expect($injector.get(target)).toEqual($injector.get(source))
