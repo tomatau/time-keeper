ae18be629411c4f93f354f20c7c936ce26b6f95c
Thomas Hudspith-Tatham <iammotivated@gmail.com>
2014-10-03 00:21:31 +0100

    returning promise in gateway

--- app/scripts/gateways/firebase/courses-gateway.js
+++ app/scripts/gateways/firebase/courses-gateway.js
@@ -1,5 +1,11 @@
 'use strict';
 angular.module('firebaseGateways')
-    .factory('firebaseCoursesGateway', function(){
-        return { add: function(){} };
+    .factory('firebaseCoursesGateway', function($q){
+        return {
+            add: function(){
+                var d = $q.defer();
+                d.resolve();
+                return d.promise;
+            }
+        };
     });
