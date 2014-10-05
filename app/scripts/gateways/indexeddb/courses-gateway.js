89e879667f198261030a8ef6b25043d65ac8f09e
Thomas Hudspith-Tatham <iammotivated@gmail.com>
2014-10-05 12:43:03 +0100

    cleaned up idbstore implementation

--- app/scripts/gateways/indexeddb/courses-gateway.js
+++ app/scripts/gateways/indexeddb/courses-gateway.js
@@ -1,11 +1,14 @@
 'use strict';
 angular.module('indexeddbGateways')
     .factory('indexeddbCoursesGateway', function($q, timeKeeperGateway){
+        var coursesStore = timeKeeperGateway({
+            storeName: 'Courses'
+        });
         return {
             add: function(course){
-                course = angular.copy(course);
                 var d = $q.defer();
-                timeKeeperGateway.put( course,
+                course = angular.copy(course);
+                coursesStore.put( course,
                     function success(id){
                         d.resolve(angular.extend(course, { id: id }));
                     }
--- app/scripts/gateways/indexeddb/gateway.js
+++ app/scripts/gateways/indexeddb/gateway.js
@@ -1,13 +1,16 @@
 'use strict';
 angular.module('indexeddbGateways', [ ])
-    .factory('timeKeeperGateway', function(ENV, $window){
-        var tkStore = new IDBStore({
-            storeName: 'TimeKeeper',
-            storePrefix: 'Tomatao-',
-            dbVersion: 1,
-            keyPath: 'id',
-            autoIncrement: true
-        });
-        if (ENV.name === 'DEV') $window.tkStore = tkStore;
-        return tkStore;
+    .factory('timeKeeperGateway', function($window, ENV){
+        var stores = [];
+        if (ENV.name === 'DEV') $window.stores = stores;
+        return function(options){
+            stores[options.storeName] = new IDBStore(angular.extend({
+                storeName: 'Root',
+                storePrefix: 'TimeKeeper-',
+                dbVersion: 1,
+                keyPath: 'id',
+                autoIncrement: true
+            }, options));
+            return stores[options.storeName];
+        };
     });
