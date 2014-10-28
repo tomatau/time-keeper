94e51b9a4af6d63812a310fc71a2907de51bfa0e
Thomas Hudspith-Tatham <iammotivated@gmail.com>
2014-10-28 18:45:08 +0000

    refactored form test

--- app/tests/form/course-form-test.js
+++ app/tests/form/course-form-test.js
@@ -28,8 +28,6 @@ describe('Course Form Test', function () {
         // console.log(compiledDir.data().$isolateScope)
     }));
 
-
-    // validation for required, pattern, number
     it('should require an entity attribute or throw', inject(function(){
         expect(compileDir).toThrow();
     }));
@@ -46,24 +44,17 @@ describe('Course Form Test', function () {
             it('should display a disable submit button', function () {
                 compileDir();
                 submitElem = compiledDir.find('[type=submit]');
-                expect(
-                    submitElem.attr('disabled')
-                ).toEqual('disabled');
+                expect( submitElem.attr('disabled') ).toEqual('disabled');
             });
         });
 
         describe('ID', function () {
             var idElem;
-
-            it('should not have an id input when not in entity', function () {
-                compileDir();
-                idElem = compiledDir.find('[name=id]');
-
-                expect( idElem.length ).toBe(0);
+            beforeEach(function () {
+                currentScope.course.id = 1;
             });
 
             it('should populate a disabled id when available', function () {
-                currentScope.course.id = 1;
                 compileDir();
                 idElem = compiledDir.find('[name=id]');
 
@@ -71,11 +62,31 @@ describe('Course Form Test', function () {
                 expect( idElem.attr('disabled') ).toBe('disabled');
             });
 
-            // validation
+            it('should be a number', function () {
+                compileDir();
+                idElem = compiledDir.find('[name=id]');
+
+                expect( idElem.attr('type') ).toBe("number");
+            });
+
+            it('should not have an id input when not in entity', function () {
+                currentScope.course.id = null;
+                compileDir();
+                idElem = compiledDir.find('[name=id]');
+
+                expect( idElem.length ).toBe(0);
+            });
         });
 
         describe('Name', function () {
             var nameElem;
+
+            it('should be required with a pattern match', function () {
+                compileDir();
+                nameElem = compiledDir.find('[name=name]');
+                expect( nameElem.attr('required') ).toBe('required');
+                expect( nameElem.attr('ng-pattern') ).toBe("/[A-Z]{3,}-\\d{3,3}$/");
+            });
 
             it('should populate the name field', function () {
                 currentScope.course.name = "test";
@@ -83,12 +94,17 @@ describe('Course Form Test', function () {
                 nameElem = compiledDir.find('[name=name]');
                 expect( nameElem.val() ).toBe( currentScope.course.name );
             });
-
-            // validation
         });
 
         describe('Version', function () {
             var versionElem;
+
+            it('should be a required number', function () {
+                compileDir();
+                versionElem = compiledDir.find('[name=version]');
+                expect( versionElem.attr('required') ).toBe('required');
+                expect( versionElem.attr('type') ).toBe("number");
+            });
             
             it('should populate the version field', function () {
                 currentScope.course.version = 2;
@@ -96,8 +112,6 @@ describe('Course Form Test', function () {
                 versionElem = compiledDir.find('[name=version]');
                 expect( Number(versionElem.val()) ).toBe( currentScope.course.version );
             });
-
-            // validation
         });
     });
 
