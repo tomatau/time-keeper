83bfe24af508db077960c9af948b77f129e2538c
Thomas Hudspith-Tatham <iammotivated@gmail.com>
2014-11-03 00:43:04 +0000

    building validators for students

--- app/scripts/forms/student/student.js
+++ app/scripts/forms/student/student.js
@@ -1,9 +1,12 @@
 'use strict';
 angular.module('forms')
-    .directive('studentForm', function(FORMSURL, formBuilder, CourseList){
+    .directive('studentForm', function(FORMSURL, formBuilder, CourseList, startDateValidator){
         return formBuilder({
             templateUrl: FORMSURL + 'student/student.tmpl.html',
             formName: 'studentForm',
+            validators: {
+                'startDate': startDateValidator
+            },
             link: function(scope, iEl, iAttr, ctrl){
                 ctrl.courseList = CourseList.get();
             }
@@ -11,14 +14,14 @@ angular.module('forms')
     });
         // Validators
         // ###########
-        // name : 
+        // name :
         // email : email
         // first-session: date, when create/future, when edit (can only change to make future)
         // session-day: weekday
         // session-time: hh:mm p
         // course: course-exists
-        // timezone: 
-        // hours: 
-        // os: 
+        // timezone:
+        // hours:
+        // os:
         // github & google: url
-        // 
+        //
